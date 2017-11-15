f7a52674aa90f3dfb1754b5e5cf98a7ab8d62106
Yuki Koyama <yuki@koyama.xyz>
2017-11-15 23:56:25 +0900

    Add a class for managing rgb(a) image data

--- unmixing/image_processing.cpp
+++ unmixing/image_processing.cpp
@@ -1,5 +1,6 @@
 #include "image_processing.h"
 #include <numeric>
+#include <QImage>
 
 namespace ImageProcessing
 {
@@ -13,6 +14,27 @@ void Image::normalize()
         for (int y = 0; y < height(); ++ y)
         {
             set_pixel(x, y, get_pixel(x, y) / sum);
+        }
+    }
+}
+
+ColorImage::ColorImage(const std::string &file_path)
+{
+    QImage q_image(QString::fromStdString(file_path));
+    const int w = q_image.width();
+    const int h = q_image.height();
+    assert(w > 0 && h > 0);
+
+    rgba_ = std::vector<Image>(4, Image(w, h));
+    for (int x = 0; x < w; ++ x)
+    {
+        for (int y = 0; y < h; ++ y)
+        {
+            const QColor q_color = q_image.pixelColor(x, y);
+            rgba_[0].set_pixel(x, y, q_color.redF());
+            rgba_[1].set_pixel(x, y, q_color.greenF());
+            rgba_[2].set_pixel(x, y, q_color.blueF());
+            rgba_[3].set_pixel(x, y, q_color.alphaF());
         }
     }
 }
@@ -39,7 +61,7 @@ Image apply_convolution(const Image& image, const Eigen::MatrixXd &kernel)
     {
         for (int y = 0; y < h; ++ y)
         {
-            Image::Value value = 0.0;
+            double value = 0.0;
             for (int kernel_x = 0; kernel_x < kernel_size; ++ kernel_x)
             {
                 for (int kernel_y = 0; kernel_y < kernel_size; ++ kernel_y)
--- unmixing/image_processing.h
+++ unmixing/image_processing.h
@@ -2,6 +2,7 @@
 #define IMAGE_PROCESSING_H
 
 #include <vector>
+#include <string>
 #include <Eigen/Core>
 
 namespace ImageProcessing
@@ -10,20 +11,18 @@ namespace ImageProcessing
 class Image
 {
 public:
-    using Value = double;
-
-    Image(int width, int height, const Value& value = 0.0) : width_(width), height_(height)
+    Image(int width, int height, double value = 0.0) : width_(width), height_(height)
     {
-        pixels = std::vector<Value>(width_ * height_, value);
+        pixels = std::vector<double>(width_ * height_, value);
     }
 
-    void set_pixel(int x, int y, const Value& value)
+    void set_pixel(int x, int y, double value)
     {
         assert(x < width() && y < height());
         pixels[y * width() + x] = value;
     }
 
-    const Value& get_pixel(int x, int y) const
+    double get_pixel(int x, int y) const
     {
         assert(x < width() && y < height());
         return pixels[y * width() + x];
@@ -38,7 +37,65 @@ private:
     int width_;
     int height_;
 
-    std::vector<Value> pixels;
+    std::vector<double> pixels;
+};
+
+class ColorImage
+{
+    ColorImage(int width, int height) :
+        width_(width),
+        height_(height)
+    {
+        rgba_ = std::vector<Image>(4, Image(width, height));
+    }
+
+    ColorImage(const std::string& file_path);
+
+    void set_rgb(int x, int y, const Eigen::Vector3d& rgb)
+    {
+        assert(x < width() && y < height());
+        for (int i : { 0, 1, 2 }) rgba_[i].set_pixel(x, y, rgb(i));
+    }
+
+    void set_rgba(int x, int y, const Eigen::Vector4d& rgba)
+    {
+        assert(x < width() && y < height());
+        for (int i : { 0, 1, 2, 3 }) rgba_[i].set_pixel(x, y, rgba(i));
+    }
+
+    Eigen::Vector3d get_rgb(int x, int y) const
+    {
+        assert(x < width() && y < height());
+        Eigen::Vector3d rgb;
+        for (int i : { 0, 1, 2 }) rgb(i) = rgba_[i].get_pixel(x, y);
+        return rgb;
+    }
+
+    Eigen::Vector4d get_rgba(int x, int y) const
+    {
+        assert(x < width() && y < height());
+        Eigen::Vector4d rgba;
+        for (int i : { 0, 1, 2, 3 }) rgba(i) = rgba_[i].get_pixel(x, y);
+        return rgba;
+    }
+
+    Image& get_r() { return rgba_[0]; }
+    Image& get_g() { return rgba_[1]; }
+    Image& get_b() { return rgba_[2]; }
+    Image& get_a() { return rgba_[3]; }
+    const Image& get_r() const { return rgba_[0]; }
+    const Image& get_g() const { return rgba_[1]; }
+    const Image& get_b() const { return rgba_[2]; }
+    const Image& get_a() const { return rgba_[3]; }
+
+    int width()  const { return width_; }
+    int height() const { return height_; }
+
+private:
+    int width_;
+    int height_;
+
+    std::vector<Image> rgba_;
 };
 
 Image apply_convolution(const Image& image, const Eigen::MatrixXd& kernel);
